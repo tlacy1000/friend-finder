c6f29f3adc369a9a9a9dcaf0381f2b9de3ab5e7d
tlacy1000 <tlacy1000@gmail.com>
2019-11-16 17:22:01 -0600

    hopefully fixed for the last time

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friends = [
+	{
+		name: "Raven",
+		photo: "https://i.imgur.com/gSMi9kl.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Beast Boy",
+		photo: "https://i.imgur.com/Cfmb4fP.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Cyborg",
+		photo: "https://i.imgur.com/HolcCc9.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Robin",
+		photo: "https://i.imgur.com/pwD9gdm.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Starfire",
+		photo: "https://i.imgur.com/t9PTxaG.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,27 +1,15 @@
-// Dependencies
-// ===========================================================
 var express = require("express");
-var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3080;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// Listener
-// ===========================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT: " + PORT);
+});
